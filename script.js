4bc3e125e467a2dc387c4d1a6535a4aec7bb27c8
Victormarks10 <aaron.victormarks10@gmail.com>
2023-06-09 20:27:49 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,7 @@
+function rafflenumber() {
+
+    const min = (Math.ceil (document.querySelector(".min").value))
+    const max = (Math.floor (document.querySelector(".max").value))
+    const result = Math.floor(Math.random() * (max - min + 1)) + min;
+    alert(result)   
+}
