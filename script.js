3714e3867ba124cb7882f40b4ba020c1eabc7a92
Victormarks10 <aaron.victormarks10@gmail.com>
2023-06-11 11:21:08 -0300

    Adicionando função verificadora de valores

--- script.js
+++ script.js
@@ -1,7 +1,17 @@
 function rafflenumber() {
 
-    const min = (Math.ceil (document.querySelector(".min").value))
-    const max = (Math.floor (document.querySelector(".max").value))
-    const result = Math.floor(Math.random() * (max - min + 1)) + min;
-    alert(result)   
-}
+    const min = (Math.ceil(document.querySelector(".min").value))
+    const max = (Math.floor(document.querySelector(".max").value))
+
+
+    if (min >= max) {
+        alert("O valor minimo precisa ser menor que o valor maximo")
+        console.log("min,max")
+    } else {
+        const result = Math.floor(Math.random() * (max - min) + min);
+        alert(result)
+
+    }
+
+
+}  
